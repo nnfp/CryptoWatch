704b65c36cfccdaf0d8fa80c08a0bdd37e1e0561
kenpaso <kengalang2@gmail.com>
2022-02-14 15:36:56 -0800

    updated before hook

--- BeforeInstallHook.sh
+++ BeforeInstallHook.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 yum update -y
-yum install nodejs14
+sudo su
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+node -v
 npm install -g npm
 npm i yarn
 npm install pm2 -g
