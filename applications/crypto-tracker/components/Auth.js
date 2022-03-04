a79f50ddaeca08ba02d1ff3bf15df3660718aafd
Madeleine De Mesa <69701993+nnfp@users.noreply.github.com>
2022-03-04 13:18:33 -0800

    styled login page (#16)

--- applications/crypto-tracker/components/Auth.js
+++ applications/crypto-tracker/components/Auth.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { supabase } from '../utils/supabaseClient'
 
+
 export default function Auth() {
   const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState('')
@@ -21,8 +22,8 @@ export default function Auth() {
   return (
     <div className="row flex flex-center">
       <div className="col-6 form-widget">
-        <h1 className="header">Supabase + Next.js</h1>
-        <p className="description">Sign in via magic link with your email below</p>
+        <h1 className="header">Welcome back!</h1>
+        <p className="description">Login in via magic link with your email</p>
         <div>
           <input
             className="inputField"
